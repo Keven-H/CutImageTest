6eed91d590b2fab6391438f839ba3eb5fce01621
JacyFox <JacyFox@foxmail.com>
2016-01-04 20:34:46 +0800

    Initial Commit

---
+++ CutImageTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CutImageTest
+//
+//  Created by Admin on 16/1/4.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CutImageTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CutImageTest
+//
+//  Created by Admin on 16/1/4.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
